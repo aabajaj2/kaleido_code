6a4450af9f7eb7793ec0ae2e9df948ed9dfb19ad
Anjani <anjani.bajaj@am.sony.com>
2018-07-16 21:38:36 -0700

    Added check for vowels

--- generate_uid.py
+++ generate_uid.py
@@ -6,8 +6,10 @@ listOfUIDs = set()
 keyValueDict = {}
 
 def get_random_string():
-    firstChar = ''.join('')
-    return ''.join(random.choice(string.ascii_uppercase+ string.digits) for i in range(int(random.choice('456'))))
+    vowels = "AEIOU"
+    consonants = "".join(set(string.ascii_uppercase) - set(vowels))
+    firstChar = random.choice(consonants)
+    return firstChar + ''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(int(random.choice('345'))))
 
 def get_value(key):
     if key in keyValueDict:
@@ -21,7 +23,7 @@ def get_value(key):
             keyValueDict[key] = newvalue
     return value
 
-test = ['1', '2', '3', '4', '6', '1', '2', '3', '33', '27']
+test = ['1', '2', '3', '4', '6', '1', '2', '3', '33', '27', '67', '10009', '10009']
 for t in test:
     print("Key:", t, "Value:", get_value(t))
 # print(generate_random_id())
